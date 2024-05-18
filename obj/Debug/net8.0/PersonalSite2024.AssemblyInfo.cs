49a1363f8addfb05ffe6439c125bd510efd9b50f
unknown <coding812@gmail.com>
2024-05-18 16:08:20 -0400

    Added glasmorphism to navbar and other areas

--- obj/Debug/net8.0/PersonalSite2024.AssemblyInfo.cs
+++ obj/Debug/net8.0/PersonalSite2024.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonalSite2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3d99f4462d1271af165f2fd944fdf5aff805673")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10b1f257797b5d95c7d78c820a56ad6380e793e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonalSite2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonalSite2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
