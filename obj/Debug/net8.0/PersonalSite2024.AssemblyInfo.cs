24662984a784840ce3c4e236c9cd0a5fb0bc0c6e
unknown <coding812@gmail.com>
2024-05-19 00:32:43 -0400

    more responsive

--- obj/Debug/net8.0/PersonalSite2024.AssemblyInfo.cs
+++ obj/Debug/net8.0/PersonalSite2024.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonalSite2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f17f774ca4aa5330c9e921ad42070b29da65025b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4b4fe9ad884acfa8fcdcee7052ddc5f2917b74b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonalSite2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonalSite2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
