f4b4fe9ad884acfa8fcdcee7052ddc5f2917b74b
unknown <coding812@gmail.com>
2024-05-19 00:28:04 -0400

    Layout adjustments

--- obj/Debug/net8.0/PersonalSite2024.AssemblyInfo.cs
+++ obj/Debug/net8.0/PersonalSite2024.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonalSite2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49a1363f8addfb05ffe6439c125bd510efd9b50f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f17f774ca4aa5330c9e921ad42070b29da65025b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonalSite2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonalSite2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
