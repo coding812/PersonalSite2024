3bd9fd452cab92eae330eca309b9b6a4a70a91eb
unknown <coding812@gmail.com>
2024-06-08 18:05:32 -0400

    Added new profile and adjusted home page layout

--- obj/Debug/net8.0/PersonalSite2024.AssemblyInfo.cs
+++ obj/Debug/net8.0/PersonalSite2024.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonalSite2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85511ef1a4d99cc74ec6006d59dcf660e1b1736d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+568766c36cff08078fb532e210ecd17d6717d094")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonalSite2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonalSite2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
