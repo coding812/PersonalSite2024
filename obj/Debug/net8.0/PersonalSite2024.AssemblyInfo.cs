d83f007930210b6debcbe1ae4efa1154dc0d06ed
unknown <coding812@gmail.com>
2024-06-19 15:02:24 -0400

    corrected some responsive issues on home page with media query

--- obj/Debug/net8.0/PersonalSite2024.AssemblyInfo.cs
+++ obj/Debug/net8.0/PersonalSite2024.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonalSite2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+734b8492d58ea97170f0bb3471233389832a3b7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e25716312ce5f03effaceb2b5b0aaee34919ec3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonalSite2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonalSite2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
