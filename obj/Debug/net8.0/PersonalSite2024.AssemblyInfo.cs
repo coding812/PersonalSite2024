347772507f7a8bbacf0aef6dd1454d8d8b8562d2
unknown <coding812@gmail.com>
2024-07-04 13:05:56 -0400

    Deleted config files

--- obj/Debug/net8.0/PersonalSite2024.AssemblyInfo.cs
+++ obj/Debug/net8.0/PersonalSite2024.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonalSite2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dafd630ac30bec8e651bc9739cf8022cfd97e84e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6839513d40d1239d884f4ff2a375d5c9ed7ad087")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonalSite2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonalSite2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
