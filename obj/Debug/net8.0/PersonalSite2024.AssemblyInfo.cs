165fb1a8cf8e73c24fbef94df08940c27581fb58
Steve <coding812@gmail.com>
2025-01-23 23:22:26 -0500

    dotnet build and publish static files

--- obj/Debug/net8.0/PersonalSite2024.AssemblyInfo.cs
+++ obj/Debug/net8.0/PersonalSite2024.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonalSite2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c66844b71f051bfed7224593c0eeca5a5127134")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ed2e507d277225c1194cd604ac8209d72d5b6f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonalSite2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonalSite2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
